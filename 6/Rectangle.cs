5eca5e6b1a7a2f5b6ddf6870e08f78b95239513c
LopatkinDA <LopatkinDA@quarta.su>
2023-05-15 14:06:51 +0300

    для пробы

--- 6/Rectangle.cs
+++ 6/Rectangle.cs
@@ -1,12 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace _6
+﻿namespace _6
 {
     internal class Rectangle
     {
+        public Rectangle Rectangle
+
     }
 }
