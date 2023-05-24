5f4dd0acded240bf1d2ce10fc98584c25e75509c
Denis Lopatkin <56197693+Meta40k@users.noreply.github.com>
2023-05-24 15:07:49 +0300

    продолжаем

---
+++ q8.2.1/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace DirectoryManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            GetCatalogs(); //   Вызов метода получения директорий
+        }
+      
+        static void GetCatalogs()
+        {
+            string dirName = @"/"; // Прописываем путь к корневой директории MacOS (для Windows скорее всего тут будет "C:\\")
+            if (Directory.Exists(dirName)) // Проверим, что директория существует
+            {
+                Console.WriteLine("Папки:"); 
+                string[] dirs = Directory.GetDirectories(dirName);  // Получим все директории корневого каталога
+              
+                foreach (string d in dirs) // Выведем их все
+                    Console.WriteLine(d);
+              
+                Console.WriteLine();
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(dirName);// Получим все файлы корневого каталога
+              
+                foreach (string s in files)   // Выведем их все
+                    Console.WriteLine(s);
+            }
+        }
+    }
+}
