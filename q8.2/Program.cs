0b2f7919e36ca62c00242509c1de158d6f5e65a3
Denis Lopatkin <56197693+Meta40k@users.noreply.github.com>
2023-05-24 15:00:06 +0300

    Начал 8 модуль

---
+++ q8.2/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.IO;
+namespace DriveManager
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // получим системные диски
+            DriveInfo[] drives = DriveInfo.GetDrives();
+ 
+            // Пробежимся по дискам и выведем их свойства
+            foreach (DriveInfo drive in drives)
+            {
+                Console.WriteLine($"Название: {drive.Name}");
+                Console.WriteLine($"Тип: {drive.DriveType}");
+                if (drive.IsReady)
+                {
+                    Console.WriteLine($"Объем: {drive.TotalSize}");
+                    Console.WriteLine($"Свободно: {drive.TotalFreeSpace}");
+                    Console.WriteLine($"Метка: {drive.VolumeLabel}");
+                }
+            }
+        }
+    }
+}
