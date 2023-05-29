7e65127848a9f20233d48b2f2059c495e36f0e55
Денис Лопаткин <meta40k@outlook.com>
2023-05-29 13:26:35 +0300

    дели пошли

---
+++ q9.1/Program.cs
@@ -0,0 +1,17 @@
+﻿public class Program 
+{
+    public delegate int SumDelegate(int a, int b, int c);
+
+    static void Main(string[] args) 
+    {
+        SumDelegate sumDelegate = Sum;
+        int result = sumDelegate(1, 10, 50);
+        Console.WriteLine(result);
+        Console.ReadKey();
+    }
+
+    static int Sum(int a, int b, int c) 
+    {
+        return a + b + c;
+    }
+}
