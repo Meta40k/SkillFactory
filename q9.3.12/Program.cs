6ae3a02ccc8e4a555f3acc0bd2d62c615178cde2
Денис Лопаткин <meta40k@outlook.com>
2023-05-29 17:34:20 +0300

    делегаты ты ещё тема

---
+++ q9.3.12/Program.cs
@@ -0,0 +1,28 @@
+﻿//Реализуйте в данном решении анонимный метод, не сломав логику приложения.
+
+namespace DelegatePractices
+{
+    class Program
+    {
+        delegate void ShowMessageDelegate(string _message);
+        static void Main(string[] args)
+        {
+            var re = delegate(string name)
+            {
+                Console.WriteLine("Hello world!");
+            };
+            
+            //re.Invoke("1");
+            
+            ShowMessageDelegate showMessageDelegate = ShowMessage;
+            showMessageDelegate.Invoke("Hello World!");
+            Console.Read();
+        }
+
+        static void ShowMessage(string _message)
+        {
+            Console.WriteLine(_message);
+        }
+        
+    }
+}
