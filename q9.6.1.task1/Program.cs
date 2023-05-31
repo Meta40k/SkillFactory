d981c3932f12a5bd99547050774262ac86d6f2da
Denis Lopatkin <56197693+Meta40k@users.noreply.github.com>
2023-05-31 13:14:22 +0300

    переделал

--- q9.6.1.task1/Program.cs
+++ q9.6.1.task1/Program.cs
@@ -6,42 +6,49 @@ namespace DelegatePractices
 {
     class Program
     {
-        public Exception exc1 = new ArgumentException();
-        public Exception exc2 = new ArgumentOutOfRangeException();
-        public Exception exc3 = new DirectoryNotFoundException();
-        public Exception exc4 = new DivideByZeroException();
-        public Exception exc5 = new MyException();
-        
         static void Main(string[] args)
         {
-            var arr = new Exception[5];
-            int nolik = 0;
-
+            var arr = new Exception[5]
+            {
+                new AggregateException(),
+                new ArgumentOutOfRangeException(),
+                new DirectoryNotFoundException(),
+                new DivideByZeroException(),
+                new MyException()
+            };
+            
+            int nolik = 0; //это нолик. Да.
 
             try
             {
                 var uu = 5 / nolik;
             }
-            catch (DivideByZeroException e)
+
+            catch (Exception ex)
             {
-                Console.WriteLine("было деление на ноль");
+                arr.Contains(ex);
             }
-            catch (DirectoryNotFoundException e)
-            {
-                Console.WriteLine("Недопустимая часть пути к каталогу.");
-            }
-            catch (ArgumentOutOfRangeException e)
-            {
-                Console.WriteLine("Аргумент находится за пределами диапазона допустимых значений.");
-            }
-            catch (ArgumentException e)
-            {
-                Console.WriteLine("Непустой аргумент, передаваемый в метод, является недопустимым.");
-            }
-            catch (MyException e)
-            {
-                Console.WriteLine("вот моё исключение исключающее иные исключения.");
-            }
+            
+            // catch (DivideByZeroException e)
+            // {
+            //     Console.WriteLine("было деление на ноль");
+            // }
+            // catch (DirectoryNotFoundException e)
+            // {
+            //     Console.WriteLine("Недопустимая часть пути к каталогу.");
+            // }
+            // catch (ArgumentOutOfRangeException e)
+            // {
+            //     Console.WriteLine("Аргумент находится за пределами диапазона допустимых значений.");
+            // }
+            // catch (ArgumentException e)
+            // {
+            //     Console.WriteLine("Непустой аргумент, передаваемый в метод, является недопустимым.");
+            // }
+            // catch (MyException e)
+            // {
+            //     Console.WriteLine("вот моё исключение исключающее иные исключения.");
+            // }
             finally
             {
                 Console.WriteLine("Всё нормально, люди в курсе! ");
