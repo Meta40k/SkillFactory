1ad4d1da5ecd4816dcf77176dfd0dea9a469057d
Denis Lopatkin <56197693+Meta40k@users.noreply.github.com>
2023-11-16 13:41:57 +0300

    q14.4

--- q14/Program.cs
+++ q14/Program.cs
@@ -4,6 +4,14 @@ internal class Program
 {
     public static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        // Задание 4 Есть массив строк:
+        string[] people = { "Анна", "Мария", "Сергей", "Алексей", "Дмитрий", "Ян" };
+        // Выбрать имена на букву А:
+        foreach (var man in people.Where(name => name.StartsWith("А")))
+        {
+            Console.WriteLine(man);
+        }
+        
+        
     }
 }
