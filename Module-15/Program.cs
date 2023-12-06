3348f5511f8c3a76aff24f832b28309fef28d752
Denis Lopatkin <56197693+Meta40k@users.noreply.github.com>
2023-12-06 17:17:37 +0300

    15.1.6

--- Module-15/Program.cs
+++ Module-15/Program.cs
@@ -2,6 +2,28 @@
 {
     private static void Main(string[] args)
     {
+        Console.WriteLine("Введите текст:");
+  
+        // читаем ввод
+        var text = Console.ReadLine();
+  
+        // сохраняем список знаков препинания и символ пробела в коллекцию
+        var punctuation = new List< char>() { ' ', ',', '.', ';', ':', '!', '?' };
+  
+        // валидация ввода
+        if (string.IsNullOrEmpty(text))
+        {
+            Console.WriteLine("Вы ввели пустой текст");
+            return;
+        }
+  
+        Console.WriteLine();
+        Console.WriteLine("Текст без знаков препинания: ");
         
+        // так как строка - это массив char, мы можем вызвать метод  except  и удалить знаки препинания
+        var noPunctuation = text.Except(punctuation).ToArray();
+  
+        // вывод
+        Console.WriteLine(noPunctuation);
     }
 }
