870a46a0c04fdd5c07c2769ce6b059440ec4918c
Denis Lopatkin <56197693+Meta40k@users.noreply.github.com>
2023-12-06 16:51:31 +0300

    15.1.4

--- Module-15/Program.cs
+++ Module-15/Program.cs
@@ -1,7 +1,24 @@
-﻿internal class Program
+﻿//Задание 15.1.4
+//Напишите метод для поиска общих букв в двух словах.
+
+using System.Security.Cryptography.X509Certificates;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
-        
+        var word1 = "соловей";
+        var word2 = "разбойник";
+
+        static List<char> Method(string word1, string word2)
+        {
+            var result = word1.Intersect(word2).ToList();
+            return result;
+        }
+
+        foreach (var c in Method(word1, word2))
+        {
+            Console.WriteLine(c);
+        }
     }
 }
