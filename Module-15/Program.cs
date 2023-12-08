844c146833e5e429cb85b10236407d10f2fc7f51
Denis Lopatkin <56197693+Meta40k@users.noreply.github.com>
2023-12-08 16:48:19 +0300

    15.4.2

--- Module-15/Program.cs
+++ Module-15/Program.cs
@@ -1,7 +1,51 @@
-﻿internal class Program
+﻿static void Main(string[] args)
 {
-    private static void Main(string[] args)
+    var departments = new List<Department>()
     {
-        
+        new Department() {Id = 1, Name = "Программирование"},
+        new Department() {Id = 2, Name = "Продажи"}
+    };
+      
+    var employees = new List<Employee>()
+    {
+        new Employee() { DepartmentId = 1, Name = "Инна", Id = 1},
+        new Employee() { DepartmentId = 1, Name = "Андрей", Id = 2},
+        new Employee() { DepartmentId = 2, Name = "Виктор ", Id = 3},
+        new Employee() { DepartmentId = 3, Name = "Альберт ", Id = 4},
+    };
+  
+    var depsWithEmployees =  departments.GroupJoin(
+        employees, // первый набор данных
+        d => d.Id, // общее свойство второго набора
+        e  => e.DepartmentId, // общее свойство первого набора
+        (d, emps ) => new  // результат выборки
+        {
+            Name = d.Name,
+            Employees = emps.Select(e=>e.Name)
+        });
+  
+    // Пробегаемся по кажлому отделу
+    foreach (var dep in depsWithEmployees)
+    {
+        Console.WriteLine(dep.Name + ":");
+      
+        // Выводим сотрудников
+        foreach (string emp in dep.Employees)
+            Console.WriteLine(emp);
+ 
+        Console.WriteLine();
     }
+}
+
+public class Employee
+{
+    public int DepartmentId { get; set; }
+    public string Name { get; set; }
+    public int Id { get; set; }
+}
+
+public class Department
+{
+    public int Id { get; set; }
+    public string Name { get; set; }
 }
